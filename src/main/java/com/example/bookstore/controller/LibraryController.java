92e33eba631ae66a8f5e6baa01bac7779b921e3d
AndreiMit23 <andrei.m5@yahoo.com>
2026-07-21 10:04:07 +0300

    implement Operations and ApiResponses for controllers

--- src/main/java/com/example/bookstore/controller/LibraryController.java
+++ src/main/java/com/example/bookstore/controller/LibraryController.java
@@ -4,6 +4,9 @@ import com.example.bookstore.module_author.AuthorResponse;
 import com.example.bookstore.module_author_book.AuthorAndBookRequest;
 import com.example.bookstore.module_author_book.BookWithAuthorResponse;
 import com.example.bookstore.service.LibraryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +21,21 @@ public class LibraryController {
     }
 
     @PostMapping
+    @Operation(summary = "Save book and author", description = "Returns a book and an author via the request body")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Book and author saved"),
+            @ApiResponse(responseCode = "404", description = "Book and author not saved")
+    })
     public BookWithAuthorResponse save(@RequestBody AuthorAndBookRequest authorAndBookRequest){
         return libraryService.saveBookWithAuthor(authorAndBookRequest);
     }
 
     @GetMapping
+    @Operation(summary = "Gets books and authors", description = "Returns a list of books and authors that are associated through id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Book and author found"),
+            @ApiResponse(responseCode = "404", description = "Book and author not found")
+    })
     public List<AuthorResponse> get(){
         return libraryService.getBookWithAuthor();
     }
